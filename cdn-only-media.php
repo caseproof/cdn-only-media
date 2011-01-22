527d2dc82873e0f7fb89745ec010d16dcc80e436
Blair Williams <supercleanse@gmail.com>
2011-01-21 22:39:39 -0700

    first commit

---
+++ cdn-only-media.php
@@ -0,0 +1,153 @@
+<?php
+/*
+Plugin Name: CDN Only Media
+Plugin URI: http://www.blairwilliams.com/cdn-only
+Description: Used in conjunction with W3 Total Cache's Amazon S3 & Cloudfront Configuration and any other plugins that utilize WordPress' native file handling API plugins.
+Version: 0.0.01
+Author: Caseproof, LLC
+Author URI: http://blairwilliams.com
+
+Copyright (C) 2011 Caseproof, LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+*/
+
+/** Currently works with W3 Total Cache's Amazon S3 & Cloudfront
+  * Configuration to bypass the local filesystem for all media files.
+  */
+class CDNOnlyMedia
+{
+  /** Constructor initializes hooks for this class. */
+  function __construct()
+  {
+    add_filter( 'wp_get_attachment_url', array('CDNOnlyMedia','modify_url'), 9, 2 );
+
+    if( self::w3tc_installed() )
+    {
+      require_once( WP_PLUGIN_DIR . '/w3-total-cache/lib/W3/Config.php' );
+      require_once( WP_PLUGIN_DIR . '/w3-total-cache/lib/S3.php' );
+
+      add_filter('filesystem_method',      array('CDNOnlyMedia','get_filesystem_method'), 10, 2);
+      add_filter('filesystem_method_file', array('CDNOnlyMedia','get_filesystem_method_file'), 10, 2);
+    }
+  }
+
+  /** Modifies the Attachment URL to use the CDN Endpoint.
+    * @param $url Original Attachment URL
+    * @param $postID ID of the attachment
+    * @return string The new URL
+    */
+  public static function modify_url($url, $postID=0)
+  {
+    $home_url = get_option('home');
+    $s3_url   = self::get_domain();
+
+    preg_match("#(/wp-content.*$)#", $url, $matches);
+    $url_path = $matches[1];
+
+    if($home_url != $s3_url)
+      return self::get_domain() . $url_path;
+  
+    return $url;
+  }
+
+  /** Gets the replacement domain name
+    * @custom Blair Williams (Caseproof, LLC)
+    * @version Blair Williams (Caseproof, LLC)
+    */
+  public static function get_domain()
+  {
+    if($w3tcconfig = self::get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $cnames = $w3tcconfig->get_array("cdn.{$engine}.cname");
+        if(isset($cnames[0]))
+          return 'http://' . $cnames[0];
+        else
+          return 'https://' . $w3tcconfig->get_string("cdn.{$engine}.bucket") . '.s3.amazonaws.com';
+      }
+    }
+  
+    return get_option('home');
+  }
+
+  /** Replace domain name
+    * @param $url Url to modify
+    * @custom Blair Williams (Caseproof, LLC)
+    * @version Blair Williams (Caseproof, LLC)
+    */
+  public static function replace_domain($url)
+  {
+    $home_url = get_option('home');
+    $s3_url   = self::get_domain();
+
+    if($home_url != $s3_url)
+      return preg_replace('#^' . preg_quote($home_url, '#') . '#', $s3_url, $url);
+
+    return $url;
+  }
+
+  function w3tc_installed()
+  {
+    if(!function_exists('is_plugin_active'))
+      require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+  
+    return is_plugin_active('w3-total-cache/w3-total-cache.php');
+  }
+
+  /** Check to see if we're using Amazon S3
+    * @return true or false
+    */
+  function using_s3()
+  {
+    $w3tcconfig = self::get_w3_config();
+    $engine = $w3tcconfig->get_string('cdn.engine');
+    return ($engine == 's3' or $engine == 'cf');
+  }
+
+  /** Gets the W3TC Config file if W3TC is active & installed
+    * @version Blair Williams (Caseproof, LLC)
+    * @return boolean W3_Config Object or false
+    */
+  function get_w3_config()
+  {
+    $w3tcconfig = & W3_Config::instance();
+    return $w3tcconfig;
+  }
+
+  function get_filesystem_method( $method, $args )
+  {
+    if(self::using_s3())
+      return 's3';
+
+    return $method;
+  }
+
+  function get_filesystem_method_file( $file, $method )
+  {
+    if($method == 's3')
+      return WP_PLUGIN_DIR . "/cdn-only-media/class-wp-filesystem-s3.php";
+    
+    return $file;
+  }
+
+}
+
+// Fire this thing up
+new CDNOnlyMedia();
+?>
---
+++ class-wp-filesystem-s3.php
@@ -0,0 +1,627 @@
+<?php
+/**
+ * WordPress S3 Filesystem.
+
+Copyright (C) 2011 Caseproof, LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+*/
+
+/**
+ * WordPress Filesystem Class for direct PHP file and folder manipulation.
+ *
+ * @uses WP_Filesystem_Base Extends class
+ */
+class WP_Filesystem_S3 extends WP_Filesystem_Base
+{
+  var $errors = null;
+  var $s3Obj = null;
+
+  /**
+   * constructor
+   *
+   * @param mixed $arg ingored argument
+   */
+  function __construct($arg)
+  {
+    $this->method = 'S3';
+    $this->errors = new WP_Error();
+  }
+
+  /**
+   * connect filesystem.
+   *
+   * @return bool Returns true on success or false on failure (always true for WP_Filesystem_Direct).
+   */
+  function connect()
+  {
+    // Create the s3Obj & connect to S3
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $this->s3Obj = new S3( $w3tcconfig->get_string( 'cdn.'.$engine.'.key' ),
+                               $w3tcconfig->get_string( 'cdn.'.$engine.'.secret' )  ); 
+
+        return ($this->s3Obj !== false);
+      }
+    }
+  
+    return false;
+  }
+
+  /**
+   * Reads entire file into a string
+   *
+   * @param string $file Name of the file to read.
+   * @return string|bool The function returns the read data or false on failure.
+   */
+  function get_contents($file)
+  {
+    // Pull down file from Amazon S3 and return it
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $bucket = $w3tcconfig->get_string("cdn.{$engine}.bucket");
+
+        $uri = $this->_get_s3_uri($file);
+  
+        if( is_object($this->s3Obj) )
+          return $s3Obj->getObject( $bucket, $uri );
+      }
+    }
+  
+    return false;
+  }
+
+  /**
+   * Reads entire file into an array
+   *
+   * @param string $file Path to the file.
+   * @return array|bool the file contents in an array or false on failure.
+   */
+  function get_contents_array($file)
+  {
+    // Pull down file from Amazon S3 and run file on it
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $bucket = $w3tcconfig->get_string("cdn.{$engine}.bucket");
+
+        $uri = $this->_get_s3_uri($file);
+  
+        if(is_object($this->s3Obj))
+        {
+          $tmp_file = $this->cwd() . '/' . uniqid( 'S3_file_' );
+          if($s3Obj->getObject( $bucket, $uri, $tmp_file ))
+          {
+            if( file_exists($tmp_file) )
+            {
+              $file_array = @file($tmp_file);
+              @unlink($tmp_file);
+              return $file_array;
+            }
+          }
+        }
+      }
+    }
+  
+    return false;
+  }
+
+  /**
+   * Write a string to a file
+   *
+   * @param string $file Remote path to the file where to write the data.
+   * @param string $contents The data to write.
+   * @param int $mode (optional) The file permissions as octal number, usually 0644.
+   * @return bool False upon failure.
+   */
+  function put_contents($file, $contents, $mode = false )
+  {
+    // push this thing up to s3
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $bucket = $w3tcconfig->get_string('cdn.'.$engine.'.bucket');
+        $uri = $this->_get_s3_uri( $file );
+  
+        if( is_object($this->s3Obj) )
+        {
+	  $s3Obj->putObject( $contents, $bucket, $uri, $this->_get_s3_perms( $mode ), array(), $this->_get_mime_type($file) );
+          
+          // We are deleting any reference of the file -- remember this is CDN ONLY
+          if(file_exists( $file ))
+            @unlink( $file );
+        }
+      }
+    }
+  
+    return false;
+  }
+
+  /**
+   * Gets the current working directory
+   *
+   * @return string|bool the current working directory on success, or false on failure.
+   */
+  function cwd()
+  {
+    // Get the bucket .. or /tmp dir?
+    return '/tmp'; // We just work out of the tmp dir
+  }
+
+  /**
+   * Change directory
+   *
+   * @param string $dir The new current directory.
+   * @return bool Returns true on success or false on failure.
+   */
+  function chdir($dir)
+  {
+    // chdir with buckets?
+    return true; // Don't worry about this now
+  }
+
+  /**
+   * Changes file group
+   *
+   * @param string $file Path to the file.
+   * @param mixed $group A group name or number.
+   * @param bool $recursive (optional) If set True changes file group recursivly. Defaults to False.
+   * @return bool Returns true on success or false on failure.
+   */
+  function chgrp($file, $group, $recursive = false)
+  {
+    // Change group permissions
+    return true; // for now we don't care what group it's in
+  }
+
+  /**
+   * Changes filesystem permissions
+   *
+   * @param string $file Path to the file.
+   * @param int $mode (optional) The permissions as octal number, usually 0644 for files, 0755 for dirs.
+   * @param bool $recursive (optional) If set True changes file group recursivly. Defaults to False.
+   * @return bool Returns true on success or false on failure.
+   */
+  function chmod($file, $mode = false, $recursive = false)
+  {
+    // Change S3 permissions
+    return true; // TODO: Make this actually work
+  }
+
+  /**
+   * Changes file owner
+   *
+   * @param string $file Path to the file.
+   * @param mixed $owner A user name or number.
+   * @param bool $recursive (optional) If set True changes file owner recursivly. Defaults to False.
+   * @return bool Returns true on success or false on failure.
+   */
+  function chown($file, $owner, $recursive = false)
+  {
+    // Change S3 owner
+    return true; // for now we don't care who the owner is
+  }
+
+  /**
+   * Gets file owner
+   *
+   * @param string $file Path to the file.
+   * @return string Username of the user.
+   */
+  function owner($file)
+  {
+    // Get Owner?
+    return "amazon"; // we don't care for now
+  }
+
+  /**
+   * Gets file permissions
+   *
+   * FIXME does not handle errors in fileperms()
+   *
+   * @param string $file Path to the file.
+   * @return string Mode of the file (last 4 digits).
+   */
+  function getchmod($file)
+  {
+    return $this->_get_file_perms(S3::ACL_PUBLIC_READ); 
+  }
+
+  function group($file)
+  {
+    return "amazon"; // we don't care for now
+  }
+
+  function copy($source, $destination, $overwrite = false)
+  {
+    // Copy the S3 file from one place to another
+
+    // push this thing up to s3
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $bucket = $w3tcconfig->get_string('cdn.'.$engine.'.bucket');
+
+        $src_uri  = $this->_get_s3_uri($source);
+        $dest_uri = $this->_get_s3_uri($destination);
+  
+        if( is_object($this->s3Obj) )
+	  $s3Obj->copyObject( $bucket, $src_uri, $bucket, $dest_uri, $this->_get_s3_perms($this->getchmod($src_uri)), array(), array( "Content-Type" => $this->_get_mime_type($dest_uri) ) );
+      }
+    }
+  }
+
+  function move($source, $destination, $overwrite = false)
+  {
+    // Move the S3 file from one place to another
+    $this->copy($source, $destination, $overwrite);
+    $this->delete($source);
+  }
+
+  function delete($file, $recursive = false)
+  {
+    // Delete an S3 file
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        // Check to make sure the file exists before trying to delete it
+        if(!$this->exists($file))
+        {
+	  $uploadpath = wp_upload_dir();
+          $file = path_join($uploadpath['basedir'], $file );
+          if(!$this->exists($file))
+            return false;
+        }
+
+        $bucket = $w3tcconfig->get_string('cdn.'.$engine.'.bucket');
+        $uri = $this->_get_s3_uri($file);
+  
+        return $s3Obj->deleteObject( $bucket, $uri );
+      }
+    }
+  
+    return false;
+  }
+
+  function exists($file)
+  {
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $bucket = $w3tcconfig->get_string('cdn.'.$engine.'.bucket');
+        $uri = $this->_get_s3_uri( $file );
+
+        $s3File = $s3Obj->getObject( $bucket, $uri );
+
+        return ($s3File !== false);
+      }
+    }
+  
+    return file_exists($file);
+  }
+
+  function is_file($file)
+  {
+    return true; // Not sure how to do this with S3
+  }
+
+  function is_dir($path)
+  {
+    return true; // Not sure how to do this with S3
+  }
+
+  function is_readable($file)
+  {
+    $chmod = $this->getchmod($file);
+    return $this->_get_s3_perms( $chmod );
+  }
+
+  function is_writable($file)
+  {
+    return true;
+  }
+
+  function atime($file)
+  {
+    return time();
+  }
+
+  function mtime($file)
+  {
+    return time();
+  }
+
+  function size($file)
+  {
+    $contents = $this->get_contents($file);
+
+    return strlen($contents);
+  }
+
+  function touch($file, $time = 0, $atime = 0)
+  {
+    return true;
+  }
+
+  function mkdir($path, $chmod = false, $chown = false, $chgrp = false)
+  {
+    return true;
+  }
+
+  function rmdir($path, $recursive = false)
+  {
+    return true;
+  }
+
+  function dirlist($path, $include_hidden = true, $recursive = false)
+  {
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+        $bucket = $w3tcconfig->get_string("cdn.{$engine}.bucket");
+    }
+    
+    return $bucket; // TODO: List the dir at some point -- for now just return the bucket name
+  }
+
+  /** Gets the W3TC Config file if W3TC is active & installed
+    * @version Blair Williams (Caseproof, LLC)
+    * @return boolean W3_Config Object or false
+    */
+  function _get_w3_config()
+  {
+    $w3tcconfig = & W3_Config::instance();
+    return $w3tcconfig;
+  }
+  
+  /** Pulls a file from S3 and saves it in the working directory. */
+  function _get_file($file)
+  {
+    if($w3tcconfig = $this->_get_w3_config())
+    {
+      $engine = $w3tcconfig->get_string('cdn.engine');
+  
+      if($engine == 's3' or $engine == 'cf')
+      {
+        $bucket = $w3tcconfig->get_string("cdn.{$engine}.bucket");
+        $uri = $this->_get_s3_uri( $file );
+  
+        if(is_object($this->s3Obj))
+        {
+          $tmp_file = $this->cwd() . '/' . basename($file);
+          $s3File = $s3Obj->getObject( $bucket, $uri, $tmp_file );
+
+          return $s3File;
+        }
+      }
+    }
+  
+    return false;
+  }
+
+  function _get_mime_type($file)
+  {
+    preg_replace('#\.([^\.]*?)$#', $file, $matches);
+    $ext = $matches[1];
+
+    if( isset($ext) and $ext and !empty($ext) )
+    {
+      $mime_array = array(
+        "ez" => "application/andrew-inset",
+        "hqx" => "application/mac-binhex40",
+        "cpt" => "application/mac-compactpro",
+        "doc" => "application/msword",
+        "bin" => "application/octet-stream",
+        "dms" => "application/octet-stream",
+        "lha" => "application/octet-stream",
+        "lzh" => "application/octet-stream",
+        "exe" => "application/octet-stream",
+        "class" => "application/octet-stream",
+        "so" => "application/octet-stream",
+        "dll" => "application/octet-stream",
+        "oda" => "application/oda",
+        "pdf" => "application/pdf",
+        "ai" => "application/postscript",
+        "eps" => "application/postscript",
+        "ps" => "application/postscript",
+        "smi" => "application/smil",
+        "smil" => "application/smil",
+        "wbxml" => "application/vnd.wap.wbxml",
+        "wmlc" => "application/vnd.wap.wmlc",
+        "wmlsc" => "application/vnd.wap.wmlscriptc",
+        "bcpio" => "application/x-bcpio",
+        "vcd" => "application/x-cdlink",
+        "pgn" => "application/x-chess-pgn",
+        "cpio" => "application/x-cpio",
+        "csh" => "application/x-csh",
+        "dcr" => "application/x-director",
+        "dir" => "application/x-director",
+        "dxr" => "application/x-director",
+        "dvi" => "application/x-dvi",
+        "spl" => "application/x-futuresplash",
+        "gtar" => "application/x-gtar",
+        "hdf" => "application/x-hdf",
+        "js" => "application/x-javascript",
+        "skp" => "application/x-koan",
+        "skd" => "application/x-koan",
+        "skt" => "application/x-koan",
+        "skm" => "application/x-koan",
+        "latex" => "application/x-latex",
+        "nc" => "application/x-netcdf",
+        "cdf" => "application/x-netcdf",
+        "sh" => "application/x-sh",
+        "shar" => "application/x-shar",
+        "swf" => "application/x-shockwave-flash",
+        "sit" => "application/x-stuffit",
+        "sv4cpio" => "application/x-sv4cpio",
+        "sv4crc" => "application/x-sv4crc",
+        "tar" => "application/x-tar",
+        "tcl" => "application/x-tcl",
+        "tex" => "application/x-tex",
+        "texinfo" => "application/x-texinfo",
+        "texi" => "application/x-texinfo",
+        "t" => "application/x-troff",
+        "tr" => "application/x-troff",
+        "roff" => "application/x-troff",
+        "man" => "application/x-troff-man",
+        "me" => "application/x-troff-me",
+        "ms" => "application/x-troff-ms",
+        "ustar" => "application/x-ustar",
+        "src" => "application/x-wais-source",
+        "xhtml" => "application/xhtml+xml",
+        "xht" => "application/xhtml+xml",
+        "zip" => "application/zip",
+        "au" => "audio/basic",
+        "snd" => "audio/basic",
+        "mid" => "audio/midi",
+        "midi" => "audio/midi",
+        "kar" => "audio/midi",
+        "mpga" => "audio/mpeg",
+        "mp2" => "audio/mpeg",
+        "mp3" => "audio/mpeg",
+        "aif" => "audio/x-aiff",
+        "aiff" => "audio/x-aiff",
+        "aifc" => "audio/x-aiff",
+        "m3u" => "audio/x-mpegurl",
+        "ram" => "audio/x-pn-realaudio",
+        "rm" => "audio/x-pn-realaudio",
+        "rpm" => "audio/x-pn-realaudio-plugin",
+        "ra" => "audio/x-realaudio",
+        "wav" => "audio/x-wav",
+        "pdb" => "chemical/x-pdb",
+        "xyz" => "chemical/x-xyz",
+        "bmp" => "image/bmp",
+        "gif" => "image/gif",
+        "ief" => "image/ief",
+        "jpeg" => "image/jpeg",
+        "jpg" => "image/jpeg",
+        "jpe" => "image/jpeg",
+        "png" => "image/png",
+        "tiff" => "image/tiff",
+        "tif" => "image/tif",
+        "djvu" => "image/vnd.djvu",
+        "djv" => "image/vnd.djvu",
+        "wbmp" => "image/vnd.wap.wbmp",
+        "ras" => "image/x-cmu-raster",
+        "pnm" => "image/x-portable-anymap",
+        "pbm" => "image/x-portable-bitmap",
+        "pgm" => "image/x-portable-graymap",
+        "ppm" => "image/x-portable-pixmap",
+        "rgb" => "image/x-rgb",
+        "xbm" => "image/x-xbitmap",
+        "xpm" => "image/x-xpixmap",
+        "xwd" => "image/x-windowdump",
+        "igs" => "model/iges",
+        "iges" => "model/iges",
+        "msh" => "model/mesh",
+        "mesh" => "model/mesh",
+        "silo" => "model/mesh",
+        "wrl" => "model/vrml",
+        "vrml" => "model/vrml",
+        "css" => "text/css",
+        "html" => "text/html",
+        "htm" => "text/html",
+        "asc" => "text/plain",
+        "txt" => "text/plain",
+        "rtx" => "text/richtext",
+        "rtf" => "text/rtf",
+        "sgml" => "text/sgml",
+        "sgm" => "text/sgml",
+        "tsv" => "text/tab-seperated-values",
+        "wml" => "text/vnd.wap.wml",
+        "wmls" => "text/vnd.wap.wmlscript",
+        "etx" => "text/x-setext",
+        "xml" => "text/xml",
+        "xsl" => "text/xml",
+        "mpeg" => "video/mpeg",
+        "mpg" => "video/mpeg",
+        "mpe" => "video/mpeg",
+        "qt" => "video/quicktime",
+        "mov" => "video/quicktime",
+        "mxu" => "video/vnd.mpegurl",
+        "avi" => "video/x-msvideo",
+        "movie" => "video/x-sgi-movie",
+        "ice" => "x-conference-xcooltalk"
+      );
+
+      if(isset($mime_array[$ext]) and $mime_array[$ext] and !empty($mime_array[$ext]))
+        return $mime_array[$ext];
+    }
+
+    return false;
+  }
+
+  /** Takes in a filesystem URI and converts it to an S3 URI. */
+  function _get_s3_uri($file)
+  {
+    preg_match("#(wp-content.*$)#", $file, $matches);
+    if( isset($matches[1]) and $matches[1] and !empty($matches[1]) )
+      return $matches[1];
+    else
+      return $file;
+  }
+
+  /** Translates a unix filesystem mode to the amazon mode */
+  function _get_s3_perms($mode = false)
+  {
+    if($mode and !($mode % 100))
+      $amazon_perms = S3::ACL_PRIVATE;
+    else if($mode and !($mode % 10));
+      $amazon_perms = S3::ACL_PUBLIC_READ;
+    else if($mode and ($mode % 10))
+      $amazon_perms = S3::ACL_PUBLIC_READ_WRITE;
+    else // Default to public readable
+      $amazon_perms = S3::ACL_PUBLIC_READ;
+
+    return $amazon_perms;
+  }
+
+  /** Translates an amazon mode into a unix filesystem mode */
+  function _get_file_perms($amazon_perms)
+  {
+    if($amazon_perms == S3::ACL_PRIVATE)
+      return 600;
+    else if($amazon_perms == S3::ACL_PUBLIC_READ)
+      return 644;
+    else // S3::ACL_PUBLIC_READ_WRITE
+      return 666;
+  }
+}
+?>
